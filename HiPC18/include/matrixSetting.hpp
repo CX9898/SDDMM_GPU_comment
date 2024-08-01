b2defbcb3013a0e99be383a61bc6072e42dc3b71
chengxing <zcx5613@gmail.com>
2024-08-01 15:43:22 +0900

    Add matrixSetting.hpp

---
+++ HiPC18/include/matrixSetting.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// The dimensions currently supported by WMMA
+const int WMMA_M = 16;
+const int WMMA_N = 16;
+const int WMMA_K = 16;
+
+// Must be multiples of WMMA supported dimensions for WMMA code to work
+const int MATRIX_M = 515 * WMMA_M;
+const int MATRIX_N = 515 * WMMA_N;
+const int MATRIX_K = 515 * WMMA_K;
+
+const int MATRIX_A_SIZE = MATRIX_M * MATRIX_K;
+const int MATRIX_B_SIZE = MATRIX_K * MATRIX_N;
+const int MATRIX_C_SIZE = MATRIX_M * MATRIX_N;
