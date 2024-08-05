345f4d7977ea8145535c363d0a661ec92bd6c900
chengxing <zcx5613@gmail.com>
2024-08-05 14:01:14 +0900

    Add enum MatrixOrder

--- HiPC18/include/Matrix.hpp
+++ HiPC18/include/Matrix.hpp
@@ -3,44 +3,9 @@
 #include <string>
 #include <vector>
 
-template<typename T>
-class Matrix {
- public:
-  Matrix() = default;
-  ~Matrix() = default;
-
-  size_t size() const {
-      return _size;
-  }
-  size_t ld() const {
-      return _leadingDimension;
-  }
-
-  void setSize(size_t size) {
-      _size = size;
-      _values.resize(size);
-  }
-  void setld(size_t ld){
-      _leadingDimension = ld;
-  }
-  void setRow(size_t row) {
-      _row = row;
-  }
-  void setCol(size_t col) {
-      _row = col;
-  }
-
-  std::vector<T> &values() {
-      return _values;
-  }
-
- private:
-  size_t _size;
-  size_t _leadingDimension;
-  size_t _row;
-  size_t _col;
-
-  std::vector<T> _values;
+enum MatrixOrder {
+  row_major,
+  col_major
 };
 
 template<typename T>
@@ -82,8 +47,54 @@ class SparseMatrix {
   std::vector<T> _values;
 };
 
+template<typename T>
+class Matrix {
+ public:
+  Matrix() = default;
+  ~Matrix() = default;
 
+  bool initializeFromSparseMatrix(const SparseMatrix<T> &matrixS);
 
+  size_t size() const {
+      return _size;
+  }
+  MatrixOrder matrixOrder() const {
+      return _matrixOrder;
+  }
+  size_t ld() const {
+      return _leadingDimension;
+  }
+  size_t row() const {
+      return _row;
+  }
+  size_t col() const {
+      return  _col;
+  }
 
+//  void setSize(size_t size) {
+//      _size = size;
+//      _values.resize(size);
+//  }
+//  void setld(size_t ld) {
+//      _leadingDimension = ld;
+//  }
+//  void setRow(size_t row) {
+//      _row = row;
+//  }
+//  void setCol(size_t col) {
+//      _row = col;
+//  }
 
+  std::vector<T> &values() {
+      return _values;
+  }
 
+ private:
+  size_t _size;
+  MatrixOrder _matrixOrder;
+  size_t _leadingDimension;
+  size_t _row;
+  size_t _col;
+
+  std::vector<T> _values;
+};
