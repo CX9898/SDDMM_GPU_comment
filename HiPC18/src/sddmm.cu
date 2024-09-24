53415048ef23fe38bd5156237b60a6ae1b895ffc
chengxing <zcx5613@gmail.com>
2024-09-24 07:14:52 +0000

    2024-09-24 commit

--- HiPC18/src/sddmm.cu
+++ HiPC18/src/sddmm.cu
@@ -315,6 +315,9 @@ void preprocessing(const Matrix S) {
     /* GPU call */
     sddmm_GPU(S, tiledS, P, W, H, comp_kernel_COO_time);
 
+    float other_time = make_CSR_time + rewrite_matrix_1D_time;
+    std::cout << "other_time = " << other_time << " ms" << std::endl;
+
     float sum_time = make_CSR_time + rewrite_matrix_1D_time + *comp_kernel_COO_time;
     std::cout << "Finished sum time = " << sum_time << " ms" << std::endl;
 
